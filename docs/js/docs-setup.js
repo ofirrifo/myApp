74f6db15e57967a3738383bbb1d25dcfe8b3b7c3
ofirrifo <ofirrifo@gmail.com>
2015-12-23 11:00:49 +0200

    docs folder

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,88 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "advertiserApp",
+      "shortName": "advertiserApp",
+      "type": "overview",
+      "moduleName": "advertiserApp",
+      "shortDescription": "advertiserApp",
+      "keywords": "advertiserapp api application main module overview"
+    },
+    {
+      "section": "api",
+      "id": "advertiserApp.controller:AdvertiserCtrl",
+      "shortName": "AdvertiserCtrl",
+      "type": "function",
+      "moduleName": "advertiserApp",
+      "shortDescription": "Create and Edit advertiser",
+      "keywords": "advertiser advertiserapp api controller create edit function"
+    },
+    {
+      "section": "api",
+      "id": "advertiserApp.controller:AdvertisersCtrl",
+      "shortName": "AdvertisersCtrl",
+      "type": "function",
+      "moduleName": "advertiserApp",
+      "shortDescription": "Display list of all advertisers",
+      "keywords": "advertiserapp advertisers api controller display function list"
+    },
+    {
+      "section": "api",
+      "id": "app.advertiserCommonService",
+      "shortName": "app.advertiserCommonService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "This service contain the common advertiser functionality",
+      "keywords": "advertiser advertisercommonservice api app common functionality service"
+    },
+    {
+      "section": "api",
+      "id": "app.advertiserEntityFactoryService",
+      "shortName": "app.advertiserEntityFactoryService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "This service contain the advertiser constructors in order to crete new advertiser instance",
+      "keywords": "advertiser advertiserentityfactoryservice api app constructors crete instance order service"
+    },
+    {
+      "section": "api",
+      "id": "app.advertiserRestService",
+      "shortName": "app.advertiserRestService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "This service help us with the CRUD action for advertiser",
+      "keywords": "action advertiser advertiserrestservice api app crud help service"
+    },
+    {
+      "section": "api",
+      "id": "app.alertService",
+      "shortName": "app.alertService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "",
+      "keywords": "alerts alertservice api app help service toaster type"
+    },
+    {
+      "section": "api",
+      "id": "app.localStorageService",
+      "shortName": "app.localStorageService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "This service help us with different functionality of browser local storage",
+      "keywords": "api app browser functionality help local localstorageservice service storage"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
